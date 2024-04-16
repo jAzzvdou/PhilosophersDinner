946f1eb3de56f2caa00d7857e09e5a65a14a222e
jazevedo <joaoazevedo592@gmail.com>
2024-04-16 17:59:31 -0300

    First Commit.

---
+++ philo/philo.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jazevedo <jazevedo@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 16:23:25 by jazevedo          #+#    #+#             */
+/*   Updated: 2024/04/16 16:26:08 by jazevedo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h> //| WRITE, USLEEP
+# include <stdlib.h> //| MALLOC, FREE, NULL
